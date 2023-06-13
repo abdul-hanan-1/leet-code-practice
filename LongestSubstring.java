0bb5069cf91735b3a1cf3b49a918fe11c5e78d92
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-06-14 00:01:21 +0500

    longest substring without repeating character (better approach)

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -26,3 +26,30 @@ class Solution {
         return longestSbstr;
     }
 }
+
+// better approach 
+
+//class Solution {
+//    public int lengthOfLongestSubstring(String s) {
+//        int longestSbstr = 0;
+//        int start = 0;
+//        int end = 0;
+//        HashMap<Character, Integer> map = new HashMap<>();
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//
+//            if (map.containsKey(c)) {
+//                start = Math.max(start, map.get(c) + 1);
+//            }
+//
+//            map.put(c, i);
+//            end = i + 1;
+//
+//            longestSbstr = Math.max(longestSbstr, end - start);
+//        }
+//
+//        return longestSbstr;
+//    }
+//}
+
